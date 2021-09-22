b1ea1e27a965513ee971da2de0e4e4739388111f
Dustin Stender <dustinpstender@gmail.com>
2021-09-22 16:06:54 +0200

    fixed the dumbest thing ever

---
+++ test/controllers/chatroom_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class ChatroomControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get chatroom_index_url
+    assert_response :success
+  end
+end
