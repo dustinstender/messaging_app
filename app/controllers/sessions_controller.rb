e86baa45020c74e57aaf3bb50c1885dee1985038
Dustin Stender <dustinpstender@gmail.com>
2021-09-23 16:12:49 +0200

    some stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:sessions][:username])
+    if user && user.authenticate(params[:sessions][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You in dawg"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Yo info ain't correct"
+      render 'new'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 end
