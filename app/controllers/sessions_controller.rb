7c1b91c5f3e51ec69555405e984ea4a8a8b83cb6
Dustin Stender <dustinpstender@gmail.com>
2021-09-23 16:30:42 +0200

    login is all good

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:sessions][:username])
-    if user && user.authenticate(params[:sessions][:password])
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You in dawg"
       redirect_to root_path
@@ -12,5 +12,11 @@ class SessionsController < ApplicationController
       flash.now[:error] = "Yo info ain't correct"
       render 'new'
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "you out mayn"
+    redirect_to login_path
   end
 end
