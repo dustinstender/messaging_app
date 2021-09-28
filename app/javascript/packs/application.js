02c88a7cba6cdfee17983d6d1482c4d8c5d21a77
Dustin Stender <dustinpstender@gmail.com>
2021-09-28 10:31:10 +0200

    messages are working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,4 +13,9 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
+$(document).on("turbolinks:load", () => {
+  $(".ui.dropdown").dropdown()
+  $(".message .close").on("click", function () {
+    $(this).closest(".message").transition("fade");
+  });
+});
